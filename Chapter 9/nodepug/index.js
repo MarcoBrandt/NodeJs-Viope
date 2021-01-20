fa8df3c39d5c543110aa4ba70adbfa6d1c79e420
MarcoBrandt <60316176+MarcoBrandt@users.noreply.github.com>
2021-01-20 15:33:32 +0200

    heroku

--- Chapter 9/nodepug/index.js
+++ Chapter 9/nodepug/index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Include helmet
+const helmet = require('helmet');
+app.use(helmet());
+
 let customers = [
     {id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
     {id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113'},
